03830c767e0abf818cefb15eb533156cf6e71ea5
arismendy56 <arismendylarancuent@gmail.com>
2025-01-03 12:05:36 -0500

    Initial project repo

---
+++ src/main/java/al/powerlifting/fitnessapi/FitnessapiApplication.java
@@ -0,0 +1,13 @@
+package al.powerlifting.fitnessapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/al/powerlifting/fitnessapi/FitnessapiApplicationTests.java
@@ -0,0 +1,13 @@
+package al.powerlifting.fitnessapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
